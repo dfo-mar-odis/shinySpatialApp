920b09b70912e64278aa56584bef076782bb5e2e
David Beauchesne <david.beauchesne@uqar.ca>
2021-02-09 22:29:56 -0500

    first round review

--- app/server.R
+++ app/server.R
@@ -1,11 +1,11 @@
 server <- function(input, output, session) {
-  
-      
+
+
   # INITIATE MAP
   map <- selectionMap()
-  edits <- callModule(editMod, leafmap = map, id = "map")  
-    
-    
+  edits <- callModule(editMod, leafmap = map, id = "map")
+
+
   # VALID AND STORE USER INFO
   valid_details <- reactive({
     # 2Bimproved
@@ -15,7 +15,7 @@ server <- function(input, output, session) {
         if (length(input$u_consent) == 3) {
           output$valid_details <- info_valid("All good!")
         } else {
-          output$valid_details <- info_valid("Please abide to terms and conditions.", 
+          output$valid_details <- info_valid("Please abide to terms and conditions in the User tab.",
           FALSE)
         }
       } else {
@@ -23,25 +23,25 @@ server <- function(input, output, session) {
          FALSE)
       }
     } else {
-      output$valid_details <- info_valid("Please enter a valid name.", 
+      output$valid_details <- info_valid("Please enter a valid name.",
         FALSE)
       }
   })
-    
+
   observeEvent(input$get_u_details, { valid_details() })
-  
-  
-  
+
+
+
   # CREATE GEOMS
   geoms <- reactiveValues(select = NULL, final = NULL)
-  
-  # GEOMS CREATED 
+
+  # GEOMS CREATED
   nb_geom <- reactive({
     n <- ifelse(is.null(geoms$select), 0, nrow(geoms$select))
     output$nb_geoms <- info_valid(glue("Number of geoms saved so far: {n}"), n)
-  })  
+  })
   observeEvent(geoms$select, { nb_geom() }, ignoreNULL = FALSE)
-    
+
   ## From bbox
   observeEvent(input$save_bbox, {
     geom <- valid_bbox(input$bbox_xmin, input$bbox_xmax, input$bbox_ymin, input$bbox_ymax, glue("bbox_{input$save_bbox}"), input$bbox_crs)
@@ -49,7 +49,7 @@ server <- function(input, output, session) {
       geoms$select <- append_geom(geoms$select, geom, input$bbox_buffer)
     }
   })
-  
+
   ## From point
   observeEvent(input$save_pt, {
     geom <- valid_points(input$pt_x, input$pt_y, glue("point_{input$save_pt}"),
@@ -58,24 +58,24 @@ server <- function(input, output, session) {
       geoms$select <- append_geom(geoms$select, geom, input$pt_buffer)
     }
   })
-    
-  ## From map 
+
+  ## From map
   observeEvent(input$save_from_map, {
-      
+
     output$from_map_not <- output$created_from_map <- renderText("")
     geom <- edits()$finished
     if (!is.null(geom)) {
       geoms$select <- append_geom(
-        geoms$select, 
-        valid_from_map(geom, glue("from_map_{input$save_from_map}")), 
+        geoms$select,
+        valid_from_map(geom, glue("from_map_{input$save_from_map}")),
         input$from_map_buffer
       )
     } else {
       output$from_map_not <- info_valid("Please use the map on the right.", FALSE)
     }
   })
-    
-  ## From import 
+
+  ## From import
   shinyjs::hide(id = "import_buffer")
   shinyjs::hide(id = "save_import")
   observeEvent(input$import_shapefile, {
@@ -84,19 +84,19 @@ server <- function(input, output, session) {
   })
   observeEvent(input$save_import, {
     geom <- valid_import(
-      input$import_shapefile$datapath, 
+      input$import_shapefile$datapath,
       glue("imported_{input$save_import}")
     )
     if (!is.null(geom)) {
       geoms$select <- append_geom(geoms$select, geom, input$import_buffer)
     }
   })
-    
-    
-    
+
+
+
   # VALID GEOM
   observeEvent(geoms$select, {
-    
+
     if (is.null(geoms$select)) {
       x <- character(0)
     } else {
@@ -109,12 +109,12 @@ server <- function(input, output, session) {
       selected = seq_along(x)
     )
   }, ignoreNULL = FALSE)
-    
+
 
   observeEvent(input$check_input_areas, {
     n <- length(input$check_input_areas)
     output$nb_geoms_selected <- info_valid(glue("Number of geoms selected: {n}"), n)
-    
+
     if (n) {
       shinyjs::show(id = "add_geoms_to_map")
       shinyjs::show(id = "valid_geoms")
@@ -122,49 +122,49 @@ server <- function(input, output, session) {
       shinyjs::hide(id = "add_geoms_to_map")
       shinyjs::hide(id = "valid_geoms")
     }
-    
+
   }, ignoreNULL = FALSE)
-  
+
   observeEvent(input$clear_map, {
     map <- selectionMap()
     callModule(editMod, leafmap = map, id = "map")
   })
-      
+
   observeEvent(input$add_geoms_to_map, {
     map <- selectionMap(geoms$select[input$check_input_areas,])
     callModule(editMod, leafmap = map, id = "map")
   }, ignoreNULL = FALSE)
-    
+
   observeEvent(input$valid_geoms, {
     n <- length(input$check_input_areas)
     output$nb_geoms_selected <- info_valid(glue("Number of geoms selected: {n}"), n, chk = TRUE)
     geoms$final <- geoms$select
   })
-  
-  
-  
-  
+
+
+
+
   # GENERATE REPORT
   values <- reactiveValues()
   values$generate <- 0
   shinyjs::hide(id = "dl_outputs")
-  
+
   ## generate report
   output$report_html <- renderUI(includeHTML("www/empty_report.html"))
   observeEvent(input$generate_rmd, {
-      
+
     if (length(input$u_consent) != 3) {
-      output$render_success <- info_valid("Please abide to terms and conditions.", FALSE)  
+      output$render_success <- info_valid("Please abide to terms and conditions in the User tab.", FALSE)
     } else {
       chk <- renderReport(
-          input = reactiveValuesToList(input), 
-          geoms = geoms$final, 
+          input = reactiveValuesToList(input),
+          geoms = geoms$final,
           fl = input$report_name
         )
       if (chk$ok) {
         output$render_success <- info_valid(chk$msg, chk$ok)
         output$report_html <- renderUI({
-        # NB Use a iframe so that the css of the report does not affect 
+        # NB Use a iframe so that the css of the report does not affect
         # the css of the app
           tags$iframe(id = "iframe_report", src = chk$html, width = '100%',
             frameborder = 'no')
@@ -175,10 +175,10 @@ server <- function(input, output, session) {
           )
         shinyjs::show(id = "dl_outputs")
       } else {
-        output$render_success <- info_valid(chk$msg, FALSE)  
+        output$render_success <- info_valid(chk$msg, FALSE)
       }
     }
 
   })
-    
+
 }
--- app/ui.R
+++ app/ui.R
@@ -5,49 +5,49 @@ lapply(list.files("R/", pattern = ".[Rr]$", full.names = TRUE), source)
 
 lay <- lapply(fl, function(x) sf::st_layers(x)$name)
 tb_ref <- data.frame(
-         id = seq_len(sum(lengths(lay))), 
+         id = seq_len(sum(lengths(lay))),
          dsn = rep(unlist(fl), lengths(lay)),
          layer = unlist(lay)
        )
 
 
 ui <- fluidPage(
-  
+
   useShinyjs(),
-  
+
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "extra.css")
   ),
-  
+
   # Application title
   titlePanel("", "BIO Spatial Shiny App"),
 
   #
   sidebarLayout(
 
-    sidebarPanel( 
-      # Title 
+    sidebarPanel(
+      # Title
       h2(div(img(src="img/bio_logo.jpg", height = 78), "Spatial Shiny App")),
-      
-      # Panels 
+
+      # Panels
       tabsetPanel(
-          
+
         # IDENTIFICATION
         tabPanel(
-          "User", 
+          "User",
           icon = icon("user-check"),
-          myhelptxt("This tab allows you to identify yourself, detail the reason(s) 
+          myhelptxt("This tab allows you to identify yourself, detail the reason(s)
           why you are generating the report and abide to terms and conditions."),
           # input
           textInput("u_name", label = "Enter your name", value = "Lorem ipsum"),
           textInput("u_email", label = "Enter your email", value = "lipsum@dfo-mpo.gc.ca"),
           textAreaInput("u_notes", label = "Provide the reason/rationale for generating the report", value = ""),
-          # terms and conditions 
+          # terms and conditions
           checkboxGroupInput("u_consent", label = "Terms and conditions",
           choiceNames = list(
-            HTML("I understand this report is for the <a href = 'https://www.dfo-mpo.gc.ca/index-eng.htm'>Department of Fisheries and Oceans Canada (DFO)</a> internal use only, and it shall not be shared with users outside DFO."),
+            HTML("I understand this report is for the <a href = 'https://www.dfo-mpo.gc.ca/index-eng.htm' target='_blank'>Department of Fisheries and Oceans Canada (DFO)</a> internal use only, and it shall not be shared with users outside DFO."),
             HTML("I will read all the caveats, disclaimers and uncertainty outlined in each section of the report."),
-            HTML("I will abide to all policies and directives of the Government of Canada, including, and not restricted to, <a href = 'https://www.canada.ca/en/government/publicservice/values.html'>values and ethics of the public service</a>.")
+            HTML("I will abide to all policies and directives of the Government of Canada, including, and not restricted to, <a href = 'https://www.canada.ca/en/government/publicservice/values.html' target='_blank'>values and ethics of the public service</a>.")
             ),
             choiceValues = list(1, 2, 3)
           ),
@@ -56,16 +56,16 @@ ui <- fluidPage(
           hspace(2),
           uiOutput("valid_details", inline = TRUE)
         ),
-            
-        # LAYER CREATION 
+
+        # LAYER CREATION
         tabPanel(
-          "Geom(s)", 
+          "Geom(s)",
           icon = icon("pencil"),
-          myhelptxt("This tab allows you to add or create areas of interest using one of the three tabs below."),
-          
-          # Three selections panels 
+          myhelptxt("This tab allows you to create or import areas of interest using one of the three tabs below."),
+
+          # Three selections panels
           tabsetPanel(
-            
+
             tabPanel(
               "Bounding box",
               myhelptxt("Use the fields below to create a bounding box."),
@@ -80,41 +80,44 @@ ui <- fluidPage(
               div(style="display: inline-block;vertical-align:top;",
                 numericInput("bbox_ymax", label = "Ymax", value = "0", min = -90, max = 90)),
               br(),
-              numericInput("bbox_crs", label = HTML("EPSG code (see <a href='https://epsg.io/'>https://epsg.io/</a>)"), value = "4326"),
+              numericInput("bbox_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326"),
               numericInput("bbox_buffer", label = "Optional buffer (m)", value = "0", min = 0),
               br(),
+              myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
               actionButton('save_bbox', 'Save', icon = icon("download"))
             ),
-            
+
             tabPanel(
               "Individual point",
-              myhelptxt("Use the fields below to create a bounding box."),
+              myhelptxt("Use the fields below to create a bounding box centered on an individual point with a buffer."),
               h4("Enter coordinates of the bounding box"),
               div(style="display: inline-block;vertical-align:top;",
                 numericInput("pt_x", label = "X", value = "0", min = -180, max = 180)),
               div(style="display: inline-block;vertical-align:top;",
                 numericInput("pt_y", label = "Y", value = "0", min = -180, max = 180)),
               br(),
-              numericInput("pt_crs", label = HTML("EPSG code (see <a href='https://epsg.io/'>https://epsg.io/</a>)"), value = "4326"),
+              numericInput("pt_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326"),
               numericInput("pt_buffer", label = "Optional buffer (m)", value = "0", min = 0),
               br(),
+              myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
               actionButton('save_pt', 'Save', icon = icon("download"))
             ),
-            
+
             tabPanel(
               "Draw from map",
-              myhelptxt("Use the interactive map on the right side to create geoms (points, lines or polygons)."),
-              h4("Create geom from map (geojson)"),
+              myhelptxt("Use the interactive map on the right side to create geometries (points, lines or polygons)."),
+              h4("Create geometries from map"),
               numericInput("from_map_buffer", label = "Optional buffer (m)", value = "0", min = 0),
+              myhelptxt("Save created geometry for use when generating report."),
               actionButton('save_from_map', 'Save from map', icon = icon("download")),
               hspace(2),
               uiOutput("created_from_map", inline = TRUE),
               uiOutput("from_map_not", inline = TRUE)
             ),
-            
+
             tabPanel(
               "Import files",
-              myhelptxt("Import your own file. The file will be read by 
+              myhelptxt("Import your own file. The file will be read by
               <a href='https://www.rdocumentation.org/packages/sf/versions/0.2-2/topics/st_read'>
               <code>sf::st_read()</code></a>
               which supports a vast variety of vector formats."),
@@ -124,19 +127,19 @@ ui <- fluidPage(
               actionButton('save_import', 'Save', icon = icon("download"))
               )
             ),
-            
+
             br(),
             uiOutput("nb_geoms")
           ),
-          
-        
+
+
         # VALID GEOMS
         tabPanel(
-          "Check", 
+          "Check",
           icon = icon("check-square"),
-          myhelptxt("This tab allows you to validate the selected geoms and add buffer to them,  buffer for the selected geoms."),
+          myhelptxt("This tab allows you to validate geometries you wish to use to generate the report and, if desired, add a buffer to selected geometries."),
           checkboxGroupInput("check_input_areas", choiceNames = "Input placeholder1", choiceValues = 1, c("none")),
-          # 
+          #
           actionButton('add_geoms_to_map', 'Add to map', icon = icon("pencil")),
           actionButton('valid_geoms', "Validate", icon = icon("check")),
           actionButton('clear_map', "Clear map", icon = icon("trash-alt")),
@@ -144,61 +147,61 @@ ui <- fluidPage(
           br(),
           uiOutput("nb_geoms_selected")
         ),
-        
-        
-        
+
+
+
         # REPORT
         tabPanel(
-          "Report", 
+          "Report",
           icon = icon("book"),
           myhelptxt("This tab allows you to customize and generate your report."),
-              
+
           tabsetPanel(
-                
+
             tabPanel(
               "Select sections",
-              myhelptxt("This tab allows you to select the relevant sections to be included in your final report."),
-              checkboxGroupInput("main_sections", 
-                label = "While numerous species have been identified in this region, only those listed by SARA, or assessed by COSEWIC and Wild species listings are summarized in this section:", 
+              myhelptxt("This tab allows you to select the sections you wish to include to your report."),
+              checkboxGroupInput("main_sections",
+                label = "While numerous species have been identified in this region, only those listed by SARA, or assessed by COSEWIC and Wild species listings are summarized in this section:",
                 choiceNames = c(
-                  "National Aquatic Species at Risk", 
-                  "Fish and Invertebrates", 
-                  "Sea turtles", 
+                  "National Aquatic Species at Risk",
+                  "Fish and Invertebrates",
+                  "Sea turtles",
                   "Cetaceans"
                 ),
                 selected = 1:4,
-                choiceValues = 1:4, 
+                choiceValues = 1:4,
               ),
-              checkboxGroupInput("extra_sections", 
-                label = "The following selection will include additional information, and species not listed by SARA, or assessed by COSEWIC and Wild species:", 
+              checkboxGroupInput("extra_sections",
+                label = "The following selection will include additional information, and species not listed by SARA, or assessed by COSEWIC and Wild species:",
                 choiceNames = c(
-                  "Ecologically or Biologically Significant Areas", 
+                  "Ecologically or Biologically Significant Areas",
                   "Additional species"
                 ),
-                choiceValues = 1:2, 
+                choiceValues = 1:2,
               ),
-                
+
             ),
-              
+
             tabPanel(
               "Generate report",
               myhelptxt("This tab allows you to generate your report"),
-              checkboxGroupInput("report_lang", 
-                label = "Select target language(s) for report", 
+              checkboxGroupInput("report_lang",
+                label = "Select target language(s) for report",
                 choiceNames = c("English", "French"),
                 choiceValues = c("EN", "FR"),
-                selected = "EN", 
+                selected = "EN",
                 inline = TRUE
               ),
               textAreaInput(
-                "u_text", 
-                label = "Subtitle", 
+                "u_text",
+                label = "Subtitle",
                 value = "Synthesis prepared by the Reproducible Reporting Team, steering committee and advisors."
               ),
               textAreaInput("u_comments", label = "Comments", value = ""),
               textInput(
-                "report_name", 
-                label = "Report filename (optional, do not specify the file extension)", 
+                "report_name",
+                label = "Report filename (optional, do not specify the file extension)",
                 value = ""
               ),
               hr(),
@@ -212,28 +215,28 @@ ui <- fluidPage(
             )
           )
           ),
-            
+
           br(),
           br(),
           hr(),
-          
-          
+
+
           HTML(
           paste0(
-            "<div id='footer_left'><a href='https://insilecoinc.github.io/'><img src='img/insileco_logo256.png' alt='inSileco logo' width = '100%'/></a></div><div id='footer_right'><h5>This shiny app was built by <a href='https://insilecoinc.github.io/'>inSileco</a> with the ", 
-            a("R package shiny", href = "https://shiny.rstudio.com/"), 
+            "<div id='footer_left'><a href='https://insilecoinc.github.io/' target='_blank'><img src='img/insileco_logo256.png' alt='inSileco logo' width = '100%'/></a></div><div id='footer_right'><h5>This shiny app was built by <a href='https://insilecoinc.github.io/' target='_blank'>inSileco</a> with the ",
+            a("R package shiny", href = "https://shiny.rstudio.com/", target = "_blank"),
             ", the source code available on ")
           ),
-          a(icon("github"), href = "https://github.com/inSilecoInc/shinySpatialApp"),
+          a(icon("github"), href = "https://github.com/inSilecoInc/shinySpatialApp", target = "_blank"),
           HTML(".</h5></div>")
-            
+
         ),
-        
+
 
     # RIGHT PANEL
     # add map
     mainPanel(
-      
+
       tabsetPanel(
         # MAP
         tabPanel(
@@ -246,12 +249,11 @@ ui <- fluidPage(
           "Report",
           icon = icon("book"),
           htmlOutput("report_html")
-        )  
+        )
       ),
     )
-    
-    
-  )
-    
-)
 
+
+  )
+
+)
