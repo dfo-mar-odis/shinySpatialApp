5ee5ac951210d07972c6170cce82d852f79decf7
Stoyel <Quentin.Stoyel@dfo-mpo.gc.ca>
2021-08-11 11:14:55 -0300

    add framework to save and restore globalenv around tests

--- tests/testthat/setup_testenv.R
+++ tests/testthat/setup_testenv.R
@@ -1,5 +1,10 @@
 
 # ---------------------PREPARE DATA--------------------------
+# save current state and start from clean slate:
+message("Saving Environment")
+save.image(file=here::here('tests/preTestEnv.RData'))
+rm(list=ls(envir = globalenv()), envir = globalenv())
+
 source(here::here("tests/testthat/help_functions_for_tests.R"))
 message("Generate new test data set")
 catch_all_output({
