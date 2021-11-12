1f49f0fd0850385fc7a3ded59264c56cc9fac102
Stoyel <Quentin.Stoyel@dfo-mpo.gc.ca>
2021-11-12 07:49:42 -0400

    add ckanr setup line

--- dataprocessing/openDataGitScript.R
+++ dataprocessing/openDataGitScript.R
@@ -1,6 +1,21 @@
 install.packages("ckanr") #you need to install the packages every time since it is a fresh container
+ckanr::ckanr_setup(url="https://open.canada.ca/data")
 
 # ---------------GIT ACTION FUNCTIONS----------------
+# get update date from package
+date_from_pkg <- function(pkgId) {
+  opendataPKG <- ckanr::package_show(pkgId)
+  pkgTime <- NULL
+  if ("date_modified" %in% names(opendataPKG)) {
+    pkgTime <- strptime(opendataPKG$date_modified, "%Y-%m-%d %H:%M:%S")  
+    pkgTime <- as.numeric(strftime(pkgTime, "%Y%m%d"))
+  } else if ("metadata_modified" %in% names(opendataPKG)) {
+    pkgTime <- strptime(opendataPKG$metadata_modified, "%Y-%m-%dT%H:%M:%S")  
+    pkgTime <- as.numeric(strftime(pkgTime, "%Y%m%d"))
+  }
+  return(pkgTime)
+}
+
 check_for_open_data_updates <- function() {
   openDataData <- read.csv(here::here("dataprocessing/openDataData.csv"))
   
@@ -15,20 +30,6 @@ check_for_open_data_updates <- function() {
                             "  \n\n")
     stop(updatePkgs$msg)
   }
-}
-
-# get update date from package
-date_from_pkg <- function(pkgId) {
-  opendataPKG <- ckanr::package_show(pkgId)
-  pkgTime <- NULL
-  if ("date_modified" %in% names(opendataPKG)) {
-    pkgTime <- strptime(opendataPKG$date_modified, "%Y-%m-%d %H:%M:%S")  
-    pkgTime <- as.numeric(strftime(pkgTime, "%Y%m%d"))
-  } else if ("metadata_modified" %in% names(opendataPKG)) {
-    pkgTime <- strptime(opendataPKG$metadata_modified, "%Y-%m-%dT%H:%M:%S")  
-    pkgTime <- as.numeric(strftime(pkgTime, "%Y%m%d"))
-  }
-  return(pkgTime)
 }
 
 # run the function
