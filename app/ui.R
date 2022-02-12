ea432ed768627e4fa69d92bba1e24c8777e84e52
David Beauchesne <david.beauchesne@uqar.ca>
2022-02-12 14:33:16 -0500

    reorganize check and geom tabs

--- app/ui.R
+++ app/ui.R
@@ -1,3 +1,4 @@
+
 ui <- fluidPage(
 
   useShinyjs(),
@@ -40,105 +41,118 @@ ui <- fluidPage(
             choiceValues = list(1, 2, 3)
           ),
           br(),
-          actionButton("get_u_details", 'Validate', icon =icon("pencil-alt")),
+          actionButton("get_u_details", 'Validate', icon =icon("check")),
           hspace(2),
           uiOutput("valid_details", inline = TRUE)
         ),
 
-        # LAYER CREATION
+        # VIEW MAP & SELECT GEOMETRIES
         tabPanel(
-          "Geometries",
-          icon = icon("pencil-alt"),
-          myhelptxt("This tab allows you to create or import areas of interest using one of the three tabs below."),
-
-          # Three selections panels
+          "View map",
+          icon = icon("map"),
+          myhelptxt("Explore the map."),
+          div(style="display: inline-block;",
+          textInput("location", label = "Enter a location", value = "Halifax")),
+          actionButton("search_loc", "Search location", icon = icon("search")),
+          br(),
+          myhelptxt("This tab allows you to create or import geometries, and validate which geometries to use to generate the report. 1. Select/create/import geometries in `Geometries`. 2. Validate geometries in `Validate`."),
+          
           tabsetPanel(
-            
+            # LAYER CREATION
             tabPanel(
-              "Bounding box",
-              myhelptxt("Use the fields below to create a bounding box."),
-              div(style="display: inline-block; vertical-align:top;",
-                numericInput("bbox_xmin", label = "Longitude min", value = "-67.183", min = -180, max = 180)),
-              div(style="display: inline-block; vertical-align:top;",
-                numericInput("bbox_xmax", label = "Longitude max", value = "-66.183", min = -180, max = 180)),
-              br(),
-              div(style="display: inline-block; vertical-align:top;",
-                numericInput("bbox_ymin", label = "Latitude min", value = "44.650", min = -90, max = 90)),
-              div(style="display: inline-block; vertical-align:top;",
-                numericInput("bbox_ymax", label = "Latitude max", value = "45.650", min = -90, max = 90)),
-              br(),
-              numericInput("bbox_buffer", label = "Optional buffer (m)", value = "10000", min = 0, max = 25000000),
-              tags$details(
-                  tags$summary("Advanced options", icon("cog")),
-                  numericInput("bbox_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326")
+              "Geometries",
+              icon = icon("pencil-alt"),
+              myhelptxt("This tab allows you to create or import areas of interest using one of the three tabs below."),
+
+              # Three selections panels
+              tabsetPanel(
+                
+                tabPanel(
+                  "Bounding box",
+                  myhelptxt("Use the fields below to create a bounding box."),
+                  div(style="display: inline-block; vertical-align:top;",
+                    numericInput("bbox_xmin", label = "Longitude min", value = "-67.183", min = -180, max = 180)),
+                  div(style="display: inline-block; vertical-align:top;",
+                    numericInput("bbox_xmax", label = "Longitude max", value = "-66.183", min = -180, max = 180)),
+                  br(),
+                  div(style="display: inline-block; vertical-align:top;",
+                    numericInput("bbox_ymin", label = "Latitude min", value = "44.650", min = -90, max = 90)),
+                  div(style="display: inline-block; vertical-align:top;",
+                    numericInput("bbox_ymax", label = "Latitude max", value = "45.650", min = -90, max = 90)),
+                  br(),
+                  numericInput("bbox_buffer", label = "Optional buffer (m)", value = "10000", min = 0, max = 25000000),
+                  tags$details(
+                      tags$summary("Advanced options", icon("cog")),
+                      numericInput("bbox_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326")
+                    ),
+                  br(),
+                  myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
+                  actionButton('save_bbox', 'Save', icon = icon("download"))
                 ),
-              br(),
-              myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
-              actionButton('save_bbox', 'Save', icon = icon("download"))
-            ),
 
-            tabPanel(
-              "Individual point",
-              myhelptxt("Use the fields below to create a bounding box centered on an individual point with a buffer."),
-              div(style="display: inline-block;vertical-align:top;",
-                numericInput("pt_x", label = "Longitude", value = "0", min = -180, max = 180)),
-              div(style="display: inline-block;vertical-align:top;",
-                numericInput("pt_y", label = "Latitude", value = "0", min = -180, max = 180)),
-              br(),
-              numericInput("pt_buffer", label = "Optional buffer (m)", value = "100000", min = 0, max = 25000000),
-              tags$details(
-                  tags$summary("Advanced options", icon("cog")),
-                  numericInput("pt_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326")
+                tabPanel(
+                  "Individual point",
+                  myhelptxt("Use the fields below to create a bounding box centered on an individual point with a buffer."),
+                  div(style="display: inline-block;vertical-align:top;",
+                    numericInput("pt_x", label = "Longitude", value = "0", min = -180, max = 180)),
+                  div(style="display: inline-block;vertical-align:top;",
+                    numericInput("pt_y", label = "Latitude", value = "0", min = -180, max = 180)),
+                  br(),
+                  numericInput("pt_buffer", label = "Optional buffer (m)", value = "100000", min = 0, max = 25000000),
+                  tags$details(
+                      tags$summary("Advanced options", icon("cog")),
+                      numericInput("pt_crs", label = HTML("Spatial projection (see EPSG code: <a href='https://epsg.io/' target='_blank'>https://epsg.io/</a>)"), value = "4326")
+                    ),
+                  br(),
+                  myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
+                  actionButton('save_pt', 'Save', icon = icon("download"))
                 ),
-              br(),
-              myhelptxt("Save bounding box. Note that geometries will not be automatically added to the map. Go to the <code>Check</code> tab to add geometries."),
-              actionButton('save_pt', 'Save', icon = icon("download"))
-            ),
 
-            tabPanel(
-              "Draw from map",
-              myhelptxt("Use the interactive map on the right side to create geometries (points, lines or polygons)."),
-              numericInput("from_map_buffer", label = "Optional buffer (m)", value = "0", min = 0),
-              myhelptxt("Save created geometry for use when generating report."),
-              actionButton('save_from_map', 'Save from map', icon = icon("download")),
-              hspace(2),
-              uiOutput("created_from_map", inline = TRUE),
-              uiOutput("from_map_not", inline = TRUE)
-            ),
+                tabPanel(
+                  "Draw from map",
+                  myhelptxt("Use the interactive map on the right side to create geometries (points, lines or polygons)."),
+                  numericInput("from_map_buffer", label = "Optional buffer (m)", value = "0", min = 0),
+                  myhelptxt("Save created geometry for use when generating report."),
+                  actionButton('save_from_map', 'Save from map', icon = icon("download")),
+                  hspace(2),
+                  uiOutput("created_from_map", inline = TRUE),
+                  uiOutput("from_map_not", inline = TRUE)
+                ),
 
-            tabPanel(
-              "Import files",
-              myhelptxt("Import your own file. The file will be read by
-              <a href='https://www.rdocumentation.org/packages/sf/versions/0.2-2/topics/st_read' target='_blank'>
-              <code>sf::st_read()</code></a>
-              which supports a vast variety of vector formats. Note that for ESRI Shapfiles, several files need to be uploaded, that is why the file selection below allow for multiple files to be uploaded at once."),
-              fileInput("import_shapefile", "Choose a file", multiple = TRUE),
-              numericInput("import_buffer", label = "Optional buffer (m)", value = "0", min = 0),
-              actionButton('save_import', 'Save', icon = icon("download"))
-              )
-            ),
+                tabPanel(
+                  "Import files",
+                  myhelptxt("Import your own file. The file will be read by
+                  <a href='https://www.rdocumentation.org/packages/sf/versions/0.2-2/topics/st_read' target='_blank'>
+                  <code>sf::st_read()</code></a>
+                  which supports a vast variety of vector formats. Note that for ESRI Shapefiles, several files need to be uploaded, that is why the file selection below allow for multiple files to be uploaded at once."),
+                  fileInput("import_shapefile", "Choose a file", multiple = TRUE),
+                  numericInput("import_buffer", label = "Optional buffer (m)", value = "0", min = 0),
+                  actionButton('save_import', 'Save', icon = icon("download"))
+                  )
+                ),
 
-            br(),
-            uiOutput("nb_geoms")
+                br(),
+                uiOutput("nb_geoms")
+              ),
+              
+              # VALID GEOMS
+              tabPanel(
+                  "Validate",
+                  icon = icon("check"),
+                  myhelptxt("This tab allows you to validate geometries you wish to use to generate a report and. It also allows you to look up a specific location."),
+                  checkboxGroupInput("check_input_areas", choiceNames = "Input placeholder1", choiceValues = 1, c("none")),
+                  actionButton('add_geoms_to_map', 'Add to map', icon = icon("pencil-alt")),
+                  actionButton('valid_geoms', "Validate", icon = icon("check")),
+                  actionButton('clear_map', "Clear map", icon = icon("trash-alt")),
+                  br(),
+                  br(),
+                  uiOutput("nb_geoms_selected")
+                ),
           ),
+        ),
+        
 
-        # VALID GEOMS
-        tabPanel(
-            "Check",
-            icon = icon("map"),
-            myhelptxt("This tab allows you to validate geometries you wish to use to generate a report and. It also allows you to look up a specific location."),
-            div(style="display: inline-block;",
-            textInput("location", label = "Enter a location", value = "Halifax")),
-            actionButton("search_loc", "Search location", icon = icon("search")),
-            br(),
-            checkboxGroupInput("check_input_areas", choiceNames = "Input placeholder1", choiceValues = 1, c("none")),
-            actionButton('add_geoms_to_map', 'Add to map', icon = icon("pencil-alt")),
-            actionButton('valid_geoms', "Validate", icon = icon("check")),
-            actionButton('clear_map', "Clear map", icon = icon("trash-alt")),
-            br(),
-            br(),
-            uiOutput("nb_geoms_selected")
-          ),
+
 
           # REPORTS  
           tabPanel(
