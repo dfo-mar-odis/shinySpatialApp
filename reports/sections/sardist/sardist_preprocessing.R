8a309c6fe151044b9fa5005c8c49aa28f866ce26
Stoyel <Quentin.Stoyel@dfo-mpo.gc.ca>
2022-08-11 15:49:54 -0300

    update preprocessing script

--- reports/sections/sardist/sardist_preprocessing.R
+++ reports/sections/sardist/sardist_preprocessing.R
@@ -8,42 +8,14 @@ loadResult <- load_rdata(c("CommonData", "sardist_rr"), regionStr)
 filter_and_union <- function(sciName, sfObj) {
   filter_sf <- dplyr::filter(sfObj, `Scientific Name`==sciName)
   out_sf <- filter_sf %>%
-  dplyr::group_by(`Scientific Name`, `Common Name`, Species_Link) %>%
+  dplyr::group_by(`Scientific Name`, `Common Name`, Species_Link, Population, SARA_Status) %>%
   dplyr::summarize(geometry = sf::st_union(geometry))
   return(out_sf)
 }
 
-# -----------SAR DIST--------------
+# -----------SAR DIST --------------
 # get opendata metadata and set missing fields:
 sardistPkgId <- "e0fabad5-9379-4077-87b9-5705f28c490b"
-<<<<<<< HEAD
-sardistResId <- "50c03ed9-a99f-4dd4-8c4e-2ad9d81432ef"
-sardistCheckDate <-  get_check_date("sardist_rr")
-
-openSardist_rr <- get_opendata_rr(sardistPkgId, sardistResId,
-                                  region_sf = region_sf,
-                                  gdbLayer = "DFO_SARA_Dist_2022_FGP_EN",
-                                  checkDate = sardistCheckDate)
-if(!is.null(openSardist_rr)) {
-  sardist_rr <- openSardist_rr
-  sardist_rr$metadata$qualityTier <- highQuality
-  sardist_rr$metadata$contact <- email_format("info\\@dfo-mpo.gc.ca")
-  sardist_rr$data_sf <- dplyr::select(sardist_rr$data_sf,
-                                      c("Common_Name_EN", "Scientific_Name",
-                                        "Species_Link", "Shape"))
-  names(sardist_rr$data_sf) <- c("Common Name", "Scientific Name",
-                                 "Species_Link", "geometry")
-  st_geometry(sardist_rr$data_sf) <- "geometry"
-  specNames <- names(table(sardist_rr$data_sf$`Scientific Name`))
-  outList <- lapply(specNames, filter_and_union, sfObj=sardist_rr$data_sf)
-  # !!! exapands the outlist in the argument, check ?"!!!"
-  sardist_rr$data_sf <- dplyr::bind_rows(!!!outList)
-
-  save(sardist_rr, file = file.path(localFileSavePath, "Open/sardist_rr.RData"))
-}
-
-
-# ------------------ESRI VERSION------------------------
 sardist_rr <- get_opendata_rr(sardistPkgId)
 
 sardist_rr$metadata$qualityTier <- highQuality
@@ -61,9 +33,10 @@ sardist_sf <- sf::st_make_valid(sardist_sf)
 
 # trim and rename columns:
 sardist_sf <- dplyr::select(sardist_sf, c("Common_Name_EN", "Scientific_Name",
-                                          "Species_Link", "Population_EN", "geoms"))
+                                          "Species_Link", "Population_EN", "SARA_Status",
+                                          "geoms"))
 names(sardist_sf) <- c("Common Name", "Scientific Name",
-                       "Species_Link", "Population", "geometry")
+                       "Species_Link", "Population", "SARA_Status", "geometry")
 st_geometry(sardist_sf) <- "geometry"
 
 # find species names to join on:
